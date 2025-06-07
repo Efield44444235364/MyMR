dd9ccc688263f1b9445f1d9e19d20c01b169773b
Efield44444235364 <efi3lx@gmail.com>
2025-06-07 20:50:02 +0700

    Update Maru loader.lua
    [ + ] success add

--- Maru loader.lua
+++ Maru loader.lua
@@ -1,5 +1,13 @@
 if game.PlaceId == 126884695634066 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/MyMR/refs/heads/main/GAG.lua"))()
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/MyMR/refs/heads/main/GAG.lua"))()
+    end)
+
+    if success then
+        print("[✅] Loaded GAG script successfully.")
+    else
+        warn("[❌] Failed to load GAG script:", err)
+    end
 else
-    print("Not GAG")
+    print("[ℹ️] Not in GAG game.")
 end
