7a14dfc6e37c37d2f4def3b71cc6d19409fd56b2
Efield44444235364 <efi3lx@gmail.com>
2025-06-07 18:01:38 +0700

    Create Maru loader.lua
    [ + ] Add Grow a Garden!!!

---
+++ Maru loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 126884695634066 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/MyMR/refs/heads/main/GAG.lua"))()
+else
+    print("Not GAG")
+end
