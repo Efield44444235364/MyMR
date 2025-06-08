5c2f827d1722281f8f233273b43dc9dfa8aa9deb
Efield44444235364 <efi3lx@gmail.com>
2025-06-08 15:33:57 +0700

    Update GAG.lua
    [ * ] Fix config

--- GAG.lua
+++ GAG.lua
@@ -1,116 +1,65 @@
+print("londing....")
 if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
 getgenv().Script_Mode = "Kaitun_Script"
-
 _G.Settings = {
-    SeedsSetting = {
+    SeedsSetting = { -- เมล็ดที่จะให้ซื้อ
         "Bamboo",
         "Carrot",
         "Orange Tulip",
         "Watermelon",
         "Pumpkin",
         "Daffodil",
-        "Tomato",
-        "Corn",
-        "Strawberry",
-        "Blueberry",
-        "Apple",
-        "Coconut",
-        "Cactus",
-        "Dragon Fruit",
-        "Mango",
-        "Grape",
         "Mushroom",
-        "Pepper",
-        "Cacao",
-        "Beanstalk",
         "Nightshade",
-        "Sunflower",
-        "Nectarine",
-        "Hive Fruit",
-        "Purple Dahlia",
-        "Pink Lily",
-        "Lilac",
-        "Foxglove",
-        "Rose",
-        "Pineapple",
-        "Peach",
-        "Raspberry",
-        "Pear",
-        "Blood Banana",
-        "Moon Melon"
+        "Beanstalk",
     },
-    
-    SprinklerSettings = {
+    SprinklerSettings = { -- ตั้งค่าสำหรับวางสปิงเกอร์
         Limit = 2,
-        StartBuyWhen = 10000000,
-        SelectSprinklers = {
-            "Basic Sprinkler", "Advanced Sprinkler",
-            "Master Sprinkler", "Godly Sprinkler"
-        }
-    },
-
-    SprinklerSettings = {
-        StartBuyWhen = 10000000,
-        SelectSprinklers = {
+        StartBuyWhen = 10000000, -- เริ่มซื้อตอนไหน
+        SelectSprinklers = { -- ซื้ออะไรบ้าง
             "Basic Sprinkler",
             "Advanced Sprinkler",
             "Master Sprinkler",
             "Godly Sprinkler"
         }
     },
-
     EggSettings = {
-        StartBuyEggWhen = 150000,
-        SelectEggs = {
+        StartBuyEggWhen = 150000, -- เริ่มซื้อตอนไหน
+        SelectEggs = { -- ซื้ออะไรบ้าง
             "Legendary Egg",
             "Mythical Egg",
             "Bug Egg"
-        }
+        },
     },
-
-    SelectHoneyStocks = {
+    SelectHoneyStocks = { -- ซื้อของอีเว้นท์
         "Hive Fruit",
         "Nectarine",
         "Flower Seed Pack",
-          "Bee Egg", 
-          "Honey Sprinkler"
+        "Bee Egg",
+        "Honey Sprinkler"
     },
-
-    DestroySettings = {
-        DestroyWhenMoney = 500000,
-        SelectTrees = {
-            "Strawberry",
-            "Blueberry",
-            "Corn",
-            "Tomato",
-            "Daffodil",
-            "Orange Tulip",
-            "Sunflower",
-            "Mushroom",
-            "Pepper"
-        }
+    DestroySettings = { 
+        DestroyWhenMoney = 500000, -- ลบต้นไม้ตอนไหน
+        SelectTrees = {"Strawberry", "Blueberry", "Corn", "Tomato"} -- ลบอะไรบ้าง
     },
-
-    Webhook = "",
-
+    Webhook = "", -- ส่งแจ้งเตือน
     PetSettings = {
-        Sell = {
+        Sell = { -- ขายอะไรบ้าง
             "Commom Egg",
             "Uncommon Egg"
         },
-        UpgradeSlots = {
+        UpgradeSlots = { -- เอาระดับไหนไปส่งเพิ่มสล็อต
             "Commom Egg",
             "Uncommon Egg"
         },
-        Equip = {
+        Equip = { -- วางสัตว์อะไรบ้าง
             "Legendary Egg",
             "Mythical Egg",
             "Bug Egg"
         }
     },
-
-    UpgradePetSlots = 2,
-    SendHoneyMachineQuests = true
+    UpgradePetSlots = 2, -- อัพสล็อตถึงระดับไหน
+    SendHoneyMachineQuests = true -- ทำเควสอีเว้นท์
 }
 
 getgenv().Key = "MARU-X1CCG-KZTL6-ZXX9-DPMYV-LJZ5"
