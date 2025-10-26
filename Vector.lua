ae6ca183bc1002108230e71e157bb7538ed0d8d9
Efield44444235364 <efi3lx@gmail.com>
2025-10-26 20:04:18 +0700

    Create Vector.lua

---
+++ Vector.lua
@@ -0,0 +1,7 @@
+
+
+
+getgenv().V = "OldKaitundiamond"
+getgenv().Webhookurl = "webhook"
+script_key = "zrvouitXLRIXNIZfSuFVBaAMcIhksZsA";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/Vector_Hub/0/v2"))()
